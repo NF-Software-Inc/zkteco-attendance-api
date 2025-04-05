af61559bcdacfd78ebd1e89445af8ad25cf9f54f
Nathanael <nf@nathanaelfrey.ca>
2025-04-05 16:08:20 -0400

    Fix publish on Windows failing to create MSIX package

--- zkteco-attendance-api/Enums/Privilege.cs
+++ zkteco-attendance-api/Enums/Privilege.cs
@@ -5,9 +5,24 @@
 	/// </summary>
 	public enum Privilege
 	{
+		/// <summary>
+		/// Default user access. Can use the device to record attendance.
+		/// </summary>
 		Default = 0,
+
+		/// <summary>
+		/// Can associate users with RFID cards, face scans, and fingerprints.
+		/// </summary>
 		Enroller = 2,
+
+		/// <summary>
+		/// Can manage the device settings.
+		/// </summary>
 		Manager = 6,
+
+		/// <summary>
+		/// Can manage the device settings, user access, and all other configuration.
+		/// </summary>
 		Admin = 14
 	}
 }
