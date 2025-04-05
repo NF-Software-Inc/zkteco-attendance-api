4fdae76068d4c754a217a8d9ea830d76cefe8ea8
Nathanael <nf@nathanaelfrey.ca>
2025-04-05 12:37:20 -0400

    Update Home component to include connection form

--- zkteco-configurator/Components/Pages/Home.razor.cs
+++ zkteco-configurator/Components/Pages/Home.razor.cs
@@ -1,18 +1,84 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using easy_blazor_bulma;
+using Microsoft.AspNetCore.Components;
+using System.ComponentModel.DataAnnotations;
 using zkteco_attendance_api;
 
 namespace zkteco_configurator.Components.Pages;
 
-public partial class Home : ComponentBase
+public sealed partial class Home : ComponentBase, IDisposable
 {
 	private readonly PageModel InputModel = new();
 	private ZkTeco? ZkTecoClock;
+	private string? ConnectionStatusMessage;
+
+	private bool DisableSubmit => string.IsNullOrWhiteSpace(InputModel.IpAddress) ||
+		InputModel.Port < 1 ||
+		InputModel.Port > 65_535 ||
+		string.IsNullOrWhiteSpace(InputModel.Password);
+
+	private readonly TooltipOptions TooltipTop = TooltipOptions.Top | TooltipOptions.HasArrow | TooltipOptions.Multiline;
+	private readonly TooltipOptions TooltipRight = TooltipOptions.Right | TooltipOptions.HasArrow | TooltipOptions.Multiline;
+
+	private void OnConnect()
+	{
+		if (ZkTecoClock != null && ZkTecoClock.IsConnected)
+		{
+			ZkTecoClock.Disconnect();
+			ZkTecoClock = null;
+		}
+
+		ZkTecoClock = new ZkTeco(InputModel.IpAddress!, InputModel.Port, InputModel.UseTcp);
+
+		if (int.TryParse(InputModel.Password, out int password) == false)
+		{
+			ConnectionStatusMessage = "Failed parsing password as an integer.";
+			ZkTecoClock = null;
+		}
+		else if (ZkTecoClock.Connect(password) == false)
+		{
+			ConnectionStatusMessage = "Failed connecting to ZKTeco clock.";
+			ZkTecoClock = null;
+		}
+		else
+		{
+			ConnectionStatusMessage = "Connected!";
+		}
+	}
 
 	private class PageModel
 	{
+		/// <summary>
+		/// IP Address of the ZKTeco device to connect to.
+		/// </summary>
+		[Display(Name = "IP Address", Description = "IP Address of the ZKTeco device to connect to.")]
 		public string? IpAddress { get; set; }
+
+		/// <summary>
+		/// Port number to use for the connection.
+		/// </summary>
+		[Display(Name = "Port", Description = "Port number to use for the connection.")]
 		public int Port { get; set; } = 4_370;
+
+		/// <summary>
+		/// Specifies whether to use TCP or UDP for the connection.
+		/// </summary>
+		[Display(Name = "Use TCP", Description = "Specifies whether to use TCP or UDP for the connection.")]
 		public bool UseTcp { get; set; } = true;
-		public string? Password { get; set; }
+
+		/// <summary>
+		/// The password to connect to the ZKTeco device.
+		/// </summary>
+		[Display(Name = "Password", Description = "The password to connect to the ZKTeco device.")]
+		public string? Password { get; set; } = "0";
+	}
+
+	/// <inheritdoc />
+	public void Dispose()
+	{
+		if (ZkTecoClock != null && ZkTecoClock.IsConnected)
+		{
+			ZkTecoClock.Disconnect();
+			ZkTecoClock = null;
+		}
 	}
 }
